0dbe76e947b434682e2209330ebdd1ace45283ba
admaraia <admaraia@gmail.com>
2023-02-23 15:17:45 +0000

    frontend_backend_v1

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
+/* Server Config */
+app.use(express.static('./assets/img'));
+app.use(express.static('./assets/css'));
+
+//Frontend Config
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
   })
 
 
